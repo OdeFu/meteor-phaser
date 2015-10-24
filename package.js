500d40a6dbdbbaf27fdb594209cfea008a5aac65
Olli Aalto <oaalto@gmail.com>
2015-10-24 13:52:17 +0300

    Finalize the fork.

--- package.js
+++ package.js
@@ -1,12 +1,12 @@
 Package.describe({
-  name: 'robertlowe:meteor-phaser',
-  summary: 'Phaser.io packaged for meteor',
-  version: '2.2.1',
-  git: 'https://github.com/robertlowe/meteor-phaser.git'
+  name: 'oaalto:meteor-phaser',
+  summary: 'Phaser.io packaged for meteor, forked from https://atmospherejs.com/robertlowe/meteor-phaser',
+  version: '2.4.4',
+  git: 'https://github.com/OdeFu/meteor-phaser.git'
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.0');
-  api.addFiles('phaser/build/phaser.js', 'client', {bare: true});
+  api.addFiles('phaser/build/phaser.js', 'client', { bare: true });
 });
 
